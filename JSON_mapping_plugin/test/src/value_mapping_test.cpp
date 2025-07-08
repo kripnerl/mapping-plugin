78529f186ecb9e3caae71cb0f2ba9ac28ad61768
Adam Parker <adam.parker@ukaea.uk>
2025-07-08 17:45:40 +0100

    Add tests for ValueMapping with 1d ints and floats; template later

--- JSON_mapping_plugin/test/src/value_mapping_test.cpp
+++ JSON_mapping_plugin/test/src/value_mapping_test.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
+#include <catch2/matchers/catch_matchers_range_equals.hpp>
 #include "map_types/value_mapping.hpp"
 #include <clientserver/initStructs.h>
 #include <nlohmann/json.hpp>
@@ -43,7 +44,7 @@ TEST_CASE("ValueMapping can be constructed from JSON", "[value_mapping]") {
     }
 }
 
-TEST_CASE("ValueMapping returns expected data for different types", "[value_mapping]") {
+TEST_CASE("ValueMapping returns expected data for different 0d types", "[value_mapping_0d]") {
 
     auto* interface = PluginInterfaceFixture::Create();
     nlohmann::json test_json = {
@@ -118,4 +119,106 @@ TEST_CASE("ValueMapping returns expected data for different types", "[value_mapp
         REQUIRE(interface->data_block->rank == 0);
         REQUIRE(*reinterpret_cast<float*>(interface->data_block->data) == 42.75);
     }
+
+    SECTION("Negative float values are correctly returned") {
+
+        test_json["VALUE"] = -42.75;
+
+        const auto& value_json = test_json.at("VALUE");
+        auto mapping = std::make_unique<ValueMapping>(value_json);
+
+        MapArguments map_args = makeMapArguments(interface);
+        auto error_code = mapping->map(map_args);
+
+        REQUIRE(error_code == 0);
+        REQUIRE(interface->data_block != nullptr);
+        REQUIRE(interface->data_block->data_type == UDA_TYPE_FLOAT);
+        REQUIRE(interface->data_block->rank == 0);
+        REQUIRE(*reinterpret_cast<float*>(interface->data_block->data) == -42.75);
+    }
 }
+
+TEST_CASE("ValueMapping returns expected data for different 1D types", "[value_mapping_1d]") {
+
+    auto* interface = PluginInterfaceFixture::Create();
+    nlohmann::json test_json = {
+        {"VALUE", {0, 0, 0}}
+    };
+
+    SECTION("1D integer arrays are correctly returned") {
+
+        std::vector<int> test_vector_1d{1, 2, 3, 4};
+        test_json["VALUE"] = test_vector_1d;
+
+        const auto& value_json = test_json.at("VALUE");
+        auto mapping = std::make_unique<ValueMapping>(value_json);
+
+        MapArguments map_args = makeMapArguments(interface);
+        auto error_code = mapping->map(map_args);
+
+        REQUIRE(error_code == 0);
+        REQUIRE(interface->data_block != nullptr);
+        REQUIRE(interface->data_block->data_type == UDA_TYPE_INT);
+        REQUIRE(interface->data_block->rank == 1);
+        const auto* data = reinterpret_cast<int*>(interface->data_block->data);
+        REQUIRE_THAT(std::vector<int>(data, data + interface->data_block->data_n), Catch::Matchers::RangeEquals(test_vector_1d));
+    }
+
+    SECTION("1D negative integer arrays are correctly returned") {
+
+        std::vector<int> test_vector_1d{-1, 2, -3, 4};
+        test_json["VALUE"] = test_vector_1d;
+
+        const auto& value_json = test_json.at("VALUE");
+        auto mapping = std::make_unique<ValueMapping>(value_json);
+
+        MapArguments map_args = makeMapArguments(interface);
+        auto error_code = mapping->map(map_args);
+
+        REQUIRE(error_code == 0);
+        REQUIRE(interface->data_block != nullptr);
+        REQUIRE(interface->data_block->data_type == UDA_TYPE_INT);
+        REQUIRE(interface->data_block->rank == 1);
+        const auto* data = reinterpret_cast<int*>(interface->data_block->data);
+        REQUIRE_THAT(std::vector<int>(data, data + interface->data_block->data_n), Catch::Matchers::RangeEquals(test_vector_1d));
+    }
+
+    SECTION("1D float arrays are correctly returned") {
+
+        std::vector<float> test_vector_1d{0.1, 0.2, 0.3, 0.4};
+        test_json["VALUE"] = test_vector_1d;
+
+        const auto& value_json = test_json.at("VALUE");
+        auto mapping = std::make_unique<ValueMapping>(value_json);
+
+        MapArguments map_args = makeMapArguments(interface);
+        auto error_code = mapping->map(map_args);
+
+        REQUIRE(error_code == 0);
+        REQUIRE(interface->data_block != nullptr);
+        REQUIRE(interface->data_block->data_type == UDA_TYPE_FLOAT);
+        REQUIRE(interface->data_block->rank == 1);
+        const auto* data = reinterpret_cast<float*>(interface->data_block->data);
+        REQUIRE_THAT(std::vector<float>(data, data + interface->data_block->data_n), Catch::Matchers::RangeEquals(test_vector_1d));
+    }
+
+    SECTION("1D negative float arrays are correctly returned") {
+
+        std::vector<float> test_vector_1d{0.1, -0.2, 0.3, -0.4};
+        test_json["VALUE"] = test_vector_1d;
+
+        const auto& value_json = test_json.at("VALUE");
+        auto mapping = std::make_unique<ValueMapping>(value_json);
+
+        MapArguments map_args = makeMapArguments(interface);
+        auto error_code = mapping->map(map_args);
+
+        REQUIRE(error_code == 0);
+        REQUIRE(interface->data_block != nullptr);
+        REQUIRE(interface->data_block->data_type == UDA_TYPE_FLOAT);
+        REQUIRE(interface->data_block->rank == 1);
+        const auto* data = reinterpret_cast<float*>(interface->data_block->data);
+        REQUIRE_THAT(std::vector<float>(data, data + interface->data_block->data_n), Catch::Matchers::RangeEquals(test_vector_1d));
+    }
+}
+
