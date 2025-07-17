0a857d0dbf2c8c5e07d07525659845da266a32bf
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2025-07-17 14:53:48 +0100

    adding additional tests

---
+++ JSON_mapping_plugin/test/src/subset_test.cpp
@@ -0,0 +1,38 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <boost/range/algorithm_ext/iota.hpp>
+#include <utils/subset.hpp>
+#include <vector>
+#include <cstddef>
+
+#include "map_types/map_arguments.hpp"
+
+// void apply_subset(TypedDataArray& input,
+//      std::optional<std::string> slice,
+//      std::optional<float> scale_factor,
+//      std::optional<float> offset);
+
+constexpr size_t array_size = 100;
+
+TEST_CASE("Test slice operations") {
+
+    // SECTION("1d array data") {
+    //     std::vector<float> data(array_size);
+    //     boost::range::iota(data, 0.);
+    //     std::vector<size_t> shape = {array_size};
+
+    //     SECTION("select single element") {
+    //         json_mapping::TypedDataArray array{data, shape, true};
+    //         json_mapping::subset::apply_subset(array, "[1]", {}, {});
+    //         REQUIRE(array.size() == 1);
+    //         REQUIRE(array.shape() == std::vector<size_t>{});
+    //     }
+
+    //     SECTION("select simple range") {
+    //         json_mapping::TypedDataArray array{data, shape, true};
+    //         json_mapping::subset::apply_subset(array, "[0:10]", {}, {});
+    //         REQUIRE(array.size() == 10);
+    //         REQUIRE(array.shape() == std::vector<size_t>{10});
+    //     }
+    // }
+}
---
+++ JSON_mapping_plugin/test/src/typed_data_array_test.cpp
@@ -0,0 +1,354 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <boost/range/algorithm/transform.hpp>
+#include <boost/range/algorithm_ext/iota.hpp>
+#include <cstddef>
+#include <gsl/gsl-lite.hpp>
+#include <iterator>
+#include <typeindex>
+#include <utils/subset.hpp>
+#include <vector>
+
+#include "map_types/map_arguments.hpp"
+
+TEST_CASE("Test create array")
+{
+    SECTION("0d array")
+    {
+        constexpr int num = 10;
+        json_mapping::TypedDataArray array{num};
+        REQUIRE(array.rank() == 0);
+        REQUIRE(array.size() == 1);
+        REQUIRE(array.shape() == std::vector<size_t>{});
+        REQUIRE(array.type_index() == std::type_index{typeid(int)});
+        REQUIRE(*reinterpret_cast<int*>(array.buffer()) == num);
+    }
+
+    SECTION("1d array")
+    {
+        constexpr size_t len = 100;
+        std::vector<float> vec(len);
+        boost::range::iota(vec, 0);
+        json_mapping::TypedDataArray array{vec, {len}, false};
+        REQUIRE(array.rank() == 1);
+        REQUIRE(array.size() == len);
+        REQUIRE(array.shape() == std::vector<size_t>{len});
+        REQUIRE(array.type_index() == std::type_index{typeid(float)});
+        REQUIRE(array.span<float>() == gsl::span{vec});
+    }
+
+    SECTION("2d array")
+    {
+        constexpr size_t dim1 = 10;
+        constexpr size_t dim2 = 10;
+        constexpr size_t len = dim1 * dim2;
+        std::vector<float> vec(len);
+        boost::range::iota(vec, 0);
+        json_mapping::TypedDataArray array{vec, {dim1, dim2}, false};
+        REQUIRE(array.rank() == 2);
+        REQUIRE(array.size() == len);
+        REQUIRE(array.shape() == std::vector<size_t>{dim1, dim2});
+        REQUIRE(array.type_index() == std::type_index{typeid(float)});
+        REQUIRE(array.span<float>() == gsl::span{vec});
+    }
+}
+
+TEST_CASE("Test array slice")
+{
+    SECTION("invalid type throws exception")
+    {
+        constexpr size_t num = 10;
+        json_mapping::TypedDataArray array{num};
+        REQUIRE_THROWS(array.slice<float>({}));
+    }
+
+    SECTION("1d array")
+    {
+        constexpr size_t len = 100;
+        std::vector<float> vec(len);
+        boost::range::iota(vec, 0);
+        json_mapping::TypedDataArray array{vec, {len}, true};
+
+        SECTION("select one element")
+        {
+            constexpr size_t element = 10;
+            std::vector<json_mapping::SubsetInfo> subsets = {json_mapping::SubsetInfo{element, element + 1, 1, len}};
+            array.slice<float>(subsets);
+
+            REQUIRE(array.rank() == 0);
+            REQUIRE(array.size() == 1);
+            REQUIRE(array.shape() == std::vector<size_t>{});
+            REQUIRE(array.type_index() == std::type_index{typeid(float)});
+            REQUIRE(*reinterpret_cast<float*>(array.buffer()) == element);
+        }
+
+        SECTION("select range")
+        {
+            constexpr size_t range_len = 10;
+            constexpr size_t start = 0;
+            constexpr size_t stop = start + range_len;
+            constexpr size_t stride = 1;
+            std::vector<json_mapping::SubsetInfo> subsets = {json_mapping::SubsetInfo{start, stop, stride, len}};
+            array.slice<float>(subsets);
+
+            REQUIRE(array.rank() == 1);
+            REQUIRE(array.size() == range_len);
+            REQUIRE(array.shape() == std::vector<size_t>{range_len});
+            REQUIRE(array.type_index() == std::type_index{typeid(float)});
+
+            std::vector<float> expected(range_len);
+            boost::range::iota(expected, 0);
+            REQUIRE(array.span<float>() == gsl::span<float>{expected});
+        }
+
+        SECTION("select strided range")
+        {
+            constexpr size_t range_len = 5;
+            constexpr size_t start = 0;
+            constexpr size_t stride = 2;
+            constexpr size_t stop = start + (range_len * stride);
+            std::vector<json_mapping::SubsetInfo> subsets = {json_mapping::SubsetInfo{start, stop, stride, len}};
+            array.slice<float>(subsets);
+
+            REQUIRE(array.rank() == 1);
+            REQUIRE(array.size() == range_len);
+            REQUIRE(array.shape() == std::vector<size_t>{range_len});
+            REQUIRE(array.type_index() == std::type_index{typeid(float)});
+
+            std::vector<float> expected(range_len);
+            boost::range::iota(expected, 0);
+            boost::range::transform(expected, expected.begin(), [](float val) { return val * stride; });
+            REQUIRE(array.span<float>() == gsl::span<float>{expected});
+        }
+    }
+
+    SECTION("2d array")
+    {
+        constexpr size_t dim1 = 10;
+        constexpr size_t dim2 = 10;
+        constexpr size_t len = dim1 * dim2;
+        std::vector<float> vec(len);
+        boost::range::iota(vec, 0);
+        json_mapping::TypedDataArray array{vec, {dim1, dim2}, true};
+
+        SECTION("select one element")
+        {
+            constexpr size_t element1 = 1;
+            constexpr size_t element2 = 3;
+            std::vector<json_mapping::SubsetInfo> subsets = {
+                json_mapping::SubsetInfo{element1, element1 + 1, 1, dim1},
+                json_mapping::SubsetInfo{element2, element2 + 1, 1, dim2},
+            };
+            array.slice<float>(subsets);
+
+            REQUIRE(array.rank() == 0);
+            REQUIRE(array.size() == 1);
+            REQUIRE(array.shape() == std::vector<size_t>{});
+            REQUIRE(array.type_index() == std::type_index{typeid(float)});
+            REQUIRE(*reinterpret_cast<float*>(array.buffer()) == (element1 * dim1) + element2);
+        }
+
+        SECTION("select 1d slice")
+        {
+            constexpr size_t element1 = 5;
+            std::vector<json_mapping::SubsetInfo> subsets = {
+                json_mapping::SubsetInfo{element1, element1 + 1, 1, dim1},
+                json_mapping::SubsetInfo{0, -1, 1, dim2},
+            };
+            array.slice<float>(subsets);
+
+            REQUIRE(array.rank() == 1);
+            REQUIRE(array.size() == 10);
+            REQUIRE(array.shape() == std::vector<size_t>{dim2});
+            REQUIRE(array.type_index() == std::type_index{typeid(float)});
+
+            std::vector<float> expected(dim2);
+            boost::range::iota(expected, dim1 * element1);
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+
+        SECTION("select range slice in first dimension")
+        {
+            constexpr size_t range_len = 3;
+            constexpr size_t start = 0;
+            constexpr size_t stop = start + range_len;
+            constexpr size_t stride = 1;
+            std::vector<json_mapping::SubsetInfo> subsets = {
+                json_mapping::SubsetInfo{start, stop, stride, dim1},
+                json_mapping::SubsetInfo{0, -1, 1, dim2},
+            };
+            array.slice<float>(subsets);
+
+            REQUIRE(array.rank() == 2);
+            REQUIRE(array.size() == dim2 * range_len);
+            REQUIRE(array.shape() == std::vector<size_t>{range_len, dim2});
+            REQUIRE(array.type_index() == std::type_index{typeid(float)});
+
+            std::vector<float> expected(dim2 * range_len);
+            boost::range::iota(expected, 0);
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+
+        SECTION("select range slice in second dimension")
+        {
+            constexpr size_t range_len = 3;
+            constexpr size_t start = 0;
+            constexpr size_t stop = start + range_len;
+            constexpr size_t stride = 1;
+            std::vector<json_mapping::SubsetInfo> subsets = {
+                json_mapping::SubsetInfo{0, -1, 1, dim1},
+                json_mapping::SubsetInfo{start, stop, stride, dim2},
+            };
+            array.slice<float>(subsets);
+
+            REQUIRE(array.rank() == 2);
+            REQUIRE(array.size() == dim1 * range_len);
+            REQUIRE(array.shape() == std::vector<size_t>{dim1, range_len});
+            REQUIRE(array.type_index() == std::type_index{typeid(float)});
+
+            std::vector<float> expected(dim1 * range_len);
+            size_t idx = 0;
+            for (int i = 0; i < dim1; ++i) {
+                for (int j = 0; j < range_len; ++j) {
+                    expected[idx] = vec[(dim1 * i) + j];
+                    ++idx;
+                }
+            }
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+
+        SECTION("select range slice in both dimensions")
+        {
+            constexpr size_t range1_len = 3;
+            constexpr size_t start1 = 0;
+            constexpr size_t stop1 = start1 + range1_len;
+            constexpr size_t stride1 = 1;
+
+            constexpr size_t range2_len = 2;
+            constexpr size_t start2 = 1;
+            constexpr size_t stop2 = start2 + range2_len;
+            constexpr size_t stride2 = 1;
+
+            std::vector<json_mapping::SubsetInfo> subsets = {
+                json_mapping::SubsetInfo{start1, stop1, stride1, dim1},
+                json_mapping::SubsetInfo{start2, stop2, stride2, dim2},
+            };
+            array.slice<float>(subsets);
+
+            REQUIRE(array.rank() == 2);
+            REQUIRE(array.size() == range1_len * range2_len);
+            REQUIRE(array.shape() == std::vector<size_t>{range1_len, range2_len});
+            REQUIRE(array.type_index() == std::type_index{typeid(float)});
+
+            std::vector<float> expected(range1_len * range2_len);
+            size_t idx = 0;
+            for (int i = start1; i < stop1; ++i) {
+                for (int j = start2; j < stop2; ++j) {
+                    expected[idx] = vec[(dim1 * i) + j];
+                    ++idx;
+                }
+            }
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+    }
+}
+
+TEST_CASE("Test array apply scale and offset")
+{
+    SECTION("invalid type throws exception")
+    {
+        constexpr int num = 10;
+        json_mapping::TypedDataArray array{num};
+        REQUIRE_THROWS(array.apply<float>(1.0, 0.0));
+    }
+
+    SECTION("0d array")
+    {
+        constexpr int num = 10;
+        json_mapping::TypedDataArray array{num};
+
+        SECTION("scale")
+        {
+            array.apply<int>(2.0, 0.0);
+            REQUIRE(*reinterpret_cast<int*>(array.buffer()) == 20);
+        }
+
+        SECTION("offset")
+        {
+            array.apply<int>(1.0, 1.0);
+            REQUIRE(*reinterpret_cast<int*>(array.buffer()) == 11);
+        }
+
+        SECTION("scale and offset")
+        {
+            array.apply<int>(2.0, 1.0);
+            REQUIRE(*reinterpret_cast<int*>(array.buffer()) == 21);
+        }
+    }
+
+    SECTION("1d array")
+    {
+        constexpr size_t len = 100;
+        std::vector<float> vec(len);
+        boost::range::iota(vec, 0);
+        json_mapping::TypedDataArray array{vec, {len}, true};
+
+        SECTION("scale")
+        {
+            array.apply<float>(2.0, 0.0);
+            std::vector<float> expected;
+            boost::range::transform(vec, std::back_inserter(expected), [](float value) { return value * 2.0; });
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+
+        SECTION("offset")
+        {
+            array.apply<float>(1.0, 1.0);
+            std::vector<float> expected;
+            boost::range::transform(vec, std::back_inserter(expected), [](float value) { return value + 1.0; });
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+
+        SECTION("scale and offset")
+        {
+            array.apply<float>(2.0, 1.0);
+            std::vector<float> expected;
+            boost::range::transform(vec, std::back_inserter(expected), [](float value) { return value * 2.0 + 1.0; });
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+    }
+
+    SECTION("2d array")
+    {
+        constexpr size_t dim1 = 10;
+        constexpr size_t dim2 = 10;
+        constexpr size_t len = dim1 * dim2;
+        std::vector<float> vec(len);
+        boost::range::iota(vec, 0);
+        json_mapping::TypedDataArray array{vec, {dim1, dim2}, true};
+
+        SECTION("scale")
+        {
+            array.apply<float>(2.0, 0.0);
+            std::vector<float> expected;
+            boost::range::transform(vec, std::back_inserter(expected), [](float value) { return value * 2.0; });
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+
+        SECTION("offset")
+        {
+            array.apply<float>(1.0, 1.0);
+            std::vector<float> expected;
+            boost::range::transform(vec, std::back_inserter(expected), [](float value) { return value + 1.0; });
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+
+        SECTION("scale and offset")
+        {
+            array.apply<float>(2.0, 1.0);
+            std::vector<float> expected;
+            boost::range::transform(vec, std::back_inserter(expected), [](float value) { return value * 2.0 + 1.0; });
+            REQUIRE(array.span<float>() == gsl::span{expected});
+        }
+    }
+}
